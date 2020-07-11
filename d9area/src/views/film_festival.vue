b95a6ac55883867317e32b999af9eaa1836a1692
zhongshuaisun <sun_zhong_shuai@163.com>
2020-07-11 22:56:59 +0800

    777777777777

--- d9area/src/views/film_festival.vue
+++ d9area/src/views/film_festival.vue
@@ -49,7 +49,41 @@
         </div>
       </div>
       <div class="FilmFestival_content">
-
+        <div class="container_main">
+          <div class="top">
+            <div class="left">
+              <img src="@/assets/images/film/top_left.png" width="760" height="497" alt />
+              <p class="left_top_p">ICF国际CG动画电影周</p>
+              <p>开启短片申报通道</p>
+            </div>
+            <div class="right">
+              <div class="right_top">
+                <img src="@/assets/images/film/top_right.png" width="389" height="240" alt />
+                <p>ICF短片申报规则说明</p>
+              </div>
+              <div class="right_bom">
+                <img src="@/assets/images/film/top_right.png" width="389" height="240" alt />
+                <p>新闻发布会</p>
+              </div>
+            </div>
+          </div>
+          <div class="bom">
+               <div class="left">
+              <div class="left_top">
+                <img src="@/assets/images/film/bom_left.png" width="389" height="240" alt />
+                <p>详细了解2020年官方评选影片</p>
+              </div>
+              <div class="left_bom">
+                <img src="@/assets/images/film/bom_left.png" width="389" height="240" alt />
+                <p>详细了解2020年官方评选影片</p>
+              </div>
+            </div>
+            <div class="right">
+              <img src="@/assets/images/film/bom_right.png" width="760" height="497" alt />
+              <p class="right_top_p">头条视频</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <Footer></Footer>
@@ -66,20 +100,21 @@ export default {
   data() {
     return {
       activeIndex: "1",
-       img_list: [
+      img_list: [
         {
-          src: require('@/assets/images/film/banner1.jpg'),
+          src: require("@/assets/images/film/banner1.jpg")
         },
         {
-          src: require('@/assets/images/film/banner2.jpg'),
-        }, {
-          src: require('@/assets/images/film/banner3.jpg'),
+          src: require("@/assets/images/film/banner2.jpg")
         },
-      ],
+        {
+          src: require("@/assets/images/film/banner3.jpg")
+        }
+      ]
     };
   },
   mounted() {
-      this.initSwiper()
+    this.initSwiper();
   },
   methods: {
     imghover() {
@@ -110,28 +145,31 @@ export default {
         case "4":
           this.$router.push("/about2");
           break;
+               case "6":
+          this.$router.push("/QA");
+          break;
         default:
           break;
       }
     },
-     initSwiper () {
+    initSwiper() {
       // this.$nextTick(() => {
       var swiper = new Swiper(".swiper-container", {
-        autoplay: false,//等同于以下设置
+        autoplay: false, //等同于以下设置
         // autoplay: {
         //   disableOnInteraction: false
         // }, //可选选项，自动滑动
         loop: true,
         speed: 1000,
         pagination: {
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
           clickable: true,
           dragSize: 30
           // type: 'progress',
         }
       });
       // });
-    },
+    }
   }
 };
 </script>
@@ -218,9 +256,82 @@ export default {
       border: 0;
     }
   }
-  .FilmFestival_content{
-      height: 2500px;
-      background: #fff;
+  .FilmFestival_content {
+    height: 2500px;
+    background: #fff;
+    padding-top: 70px;
+    .top {
+           height: 650px;
+      .left {
+        float: left;
+        width: 760px;
+           height: 650px;
+        
+           p{
+               margin-top: 15px;
+            // height: 30px;
+                 font-size: 32px;
+            font-weight: 400;
+            color: rgba(0, 0, 0, 1); 
+           }
+              .left_top_p{
+               margin-top: 63px;
+           }
+      }
+      .right {
+        float: right;
+        width: 389px;
+        height: 650px;
+        p{
+            margin-top: 22px;
+            height: 30px;
+                 font-size: 32px;
+            font-weight: 400;
+            color: rgba(0, 0, 0, 1);
+        }
+        .right_bom {
+            margin-top: 58px;
+         
+        }
+      }
+    }
+    .bom {
+        margin-top: 80px;
+           height: 650px;
+            .left {
+        float: left;
+        width: 389px;
+        height: 650px;
+        p{
+            margin-top: 22px;
+            height: 30px;
+                 font-size: 32px;
+            font-weight: 400;
+            color: rgba(0, 0, 0, 1);
+        }
+        .left_bom {
+            margin-top: 58px;
+         
+        }
+      }
+      .right {
+        float: right;
+        width: 760px;
+           height: 650px;
+        
+           p{
+               margin-top: 15px;
+            // height: 30px;
+                 font-size: 32px;
+            font-weight: 400;
+            color: rgba(0, 0, 0, 1); 
+           }
+              .right_top_p{
+               margin-top: 63px;
+           }
+      }
+     
+    }
   }
 }
 </style>
