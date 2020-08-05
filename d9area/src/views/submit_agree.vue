8ca185f224d476bae812b1917b4ca115611b7146
zhongshuaisun <zhongshuaisun@126.com>
2020-08-05 12:40:38 +0800

    1232132

--- d9area/src/views/submit_agree.vue
+++ d9area/src/views/submit_agree.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="body_box" style="height: auto;">
+  <div class="body_box"
+       style="height: auto;">
     <Nav :mainnav="mainnav"></Nav>
     <div class="submit_agree">
       <div class="agree_banner">
-        <img src="@/assets/images/film/film_show_bg1.jpg" alt />
+        <img src="@/assets/images/film/film_show_bg1.jpg"
+             alt />
       </div>
       <div class="main_container">
         <!-- <div class="big_title">D9区国际CG动画电影周章程</div>
@@ -12,9 +14,7 @@
           <p class="titles">第一条</p>
           <p class="paragraph">本章程适用于报名参加第一届D9区国际CG动画电影周（以下简称“ICF”）主竞赛单元的影片。</p>
           <p class="titles">第二条</p>
-          <p
-            class="paragraph"
-          >D9区国际CG动画电影周，是由xxx批准，中国电影美术学会CG艺术专业委员会与北京国际设计周联合主办的CG专业领域的大型竞赛型电影周。电影周的宗旨是评选并奖励优秀CG短片的创作，发现、扶持和激励有理想、有激情、有创造力的短片影视、动画团队和人才，孵化优质短片项目，促进国际文化交流与合作，创建CG短片创作、汇聚、交流、交易和投融资的国际性品牌平台。</p>
+          <p class="paragraph">D9区国际CG动画电影周，是由xxx批准，中国电影美术学会CG艺术专业委员会与北京国际设计周联合主办的CG专业领域的大型竞赛型电影周。电影周的宗旨是评选并奖励优秀CG短片的创作，发现、扶持和激励有理想、有激情、有创造力的短片影视、动画团队和人才，孵化优质短片项目，促进国际文化交流与合作，创建CG短片创作、汇聚、交流、交易和投融资的国际性品牌平台。</p>
           <!-- 第三条 -->
           <p class="titles">第三条</p>
           <p class="paragraph3">ICF每年12月举行。</p>
@@ -47,31 +47,21 @@
           <p class="standard4">b.所有图片jpg/gif/png格式，像素小于10M；</p>
           <p class="standard_4">（2）VR短片：</p>
           <p class="standard4">
-            a.视频360°全景mp4格式，容量小于4G，要求4K，可在PC或MAC平台上
-            使用UtoVR、VR Player、PotPlayer 、爱奇艺VR等支持本地播放的VR播放器
-            播放器观看；
+            a.视频360°全景mp4格式，容量小于4G，要求4K，可在PC或MAC平台上 使用UtoVR、VR Player、PotPlayer 、爱奇艺VR等支持本地播放的VR播放器 播放器观看；
           </p>
           <p class="standard4">b. 所有图片jpg/gif/png格式，像素小于10M；</p>
 
-          <p
-            class="standard"
-          >8．申报者须保证拥有该作品的全部版权或持有版权方授权许可证明，证明其有权在ICF上宣传、放映和参加相关活动。因申报作品著作权、商标权及其他知识产权所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
-          <p
-            class="standard"
-          >9．申报者须确保作品的申报符合所在国家/地区的法律法规要求，因申报方违反其所在国家/地区的法律法规所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
+          <p class="standard">8．申报者须保证拥有该作品的全部版权或持有版权方授权许可证明，证明其有权在ICF上宣传、放映和参加相关活动。因申报作品著作权、商标权及其他知识产权所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
+          <p class="standard">9．申报者须确保作品的申报符合所在国家/地区的法律法规要求，因申报方违反其所在国家/地区的法律法规所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
 
           <p class="titles">第五条</p>
           <p class="paragraph">请登录ICF网站www.di9qu.com，通过“影片申报”模块进行在线报名，并逐项填写申报影片的必填项信息。</p>
           <p class="paragraph">申报影片的截止日期为2020年11月30日。</p>
-          <p
-            class="paragraph"
-          >ICF只接受官网在线申报影片，不接受其他移动储存介质或在线样片链接。申报影片的所有资料等应按主办方的要求提交，申报者同意并授权主办方无偿使用申报作品提供的所有材料，进行推广宣传、展映活动、媒体的各种媒介宣传使用的权利，ICF组委会承诺：申报影片的作有资料仅限于对本活动和本影片的宣传（如主办方要求，申报者需承诺提供相应授权文件）；</p>
+          <p class="paragraph">ICF只接受官网在线申报影片，不接受其他移动储存介质或在线样片链接。申报影片的所有资料等应按主办方的要求提交，申报者同意并授权主办方无偿使用申报作品提供的所有材料，进行推广宣传、展映活动、媒体的各种媒介宣传使用的权利，ICF组委会承诺：申报影片的作有资料仅限于对本活动和本影片的宣传（如主办方要求，申报者需承诺提供相应授权文件）；</p>
 
           <p class="titles">第六条</p>
           <p class="paragraph">申报作品提交成功后，组委会原则上不再接受新的版本。如果报名信息有重大变化，请及时联系组委会；</p>
-          <p
-            class="paragraph"
-          >申报者所提交的所有材料均不可退还，请务必保留原件，申报影片在主竞赛单元入围后，即不得以任何理由擅自撤回影片，退出评选。如需撤回影片，ICF组委会有权根据实际情况决定是否接受。如因申报方提出撤回影片申请给ICF或ICF组委会造成任何损失的，申报方应当赔偿由此造成的一切经济损失及名誉损失，包括但不限于支付赔偿金，向电影周出具书面致歉信及在公众平台公开道歉等。最终入围选手须给予主办方至少一次在北京区放映影片的权利。</p>
+          <p class="paragraph">申报者所提交的所有材料均不可退还，请务必保留原件，申报影片在主竞赛单元入围后，即不得以任何理由擅自撤回影片，退出评选。如需撤回影片，ICF组委会有权根据实际情况决定是否接受。如因申报方提出撤回影片申请给ICF或ICF组委会造成任何损失的，申报方应当赔偿由此造成的一切经济损失及名誉损失，包括但不限于支付赔偿金，向电影周出具书面致歉信及在公众平台公开道歉等。最终入围选手须给予主办方至少一次在北京区放映影片的权利。</p>
 
           <p class="titles">第七条</p>
           <p class="paragraph">入围的影片，申报方需提供完整的中英文字幕台本。</p>
@@ -79,9 +69,7 @@
           <p class="paragraph">影片放映时配有电子中文字幕显示，英译中的翻译和字幕机操作由电影周负责。</p>
 
           <p class="titles">第八条</p>
-          <p
-            class="paragraph"
-          >ICF邀请国内外电影专家组成主竞赛单元的评审委员会，对申报影片进行评选。入围影片将在颁奖典礼前公布，获奖作品的最终结果将在颁奖典礼上公布。 评委不能与竞赛影片的制作或商业开发有关。</p>
+          <p class="paragraph">ICF邀请国内外电影专家组成主竞赛单元的评审委员会，对申报影片进行评选。入围影片将在颁奖典礼前公布，获奖作品的最终结果将在颁奖典礼上公布。 评委不能与竞赛影片的制作或商业开发有关。</p>
 
           <p class="standard">评委会将评出下列奖项：</p>
           <p class="standard">最佳短片奖</p>
@@ -96,9 +84,7 @@
           <p class="standard">最佳技术贡献奖</p>
 
           <p class="titles">第九条</p>
-          <p
-            class="paragraph"
-          >入围ICF竞赛单元作品的导演（制片人或制作团队的其他代表）将被邀请到北京参加为期一周的电影周及其他相关活动，包括映后谈、ICF论坛、青年导演扶持计划、颁奖典礼等。最终入围者需要独立承担赴中旅游签和旅游保险，主办方可根据需求提供邀请函。</p>
+          <p class="paragraph">入围ICF竞赛单元作品的导演（制片人或制作团队的其他代表）将被邀请到北京参加为期一周的电影周及其他相关活动，包括映后谈、ICF论坛、青年导演扶持计划、颁奖典礼等。最终入围者需要独立承担赴中旅游签和旅游保险，主办方可根据需求提供邀请函。</p>
 
           <p class="titles">第十一条</p>
           <p class="paragraph">凡申报影片参加D9区国际CG动画电影周，即被视为承认并接受本章程。申报方有义务确保影片选送ICF的合法性。</p>
@@ -111,8 +97,11 @@
           <p class="paragraph13">邮箱：111</p>
           <p class="paragraph13">电话：111</p>
         </div>
-
-        <div></div>
+        <p class="remember">
+          <el-checkbox v-model="checked">我已阅读并同意遵守条例</el-checkbox>
+        </p>
+        <p class="submit_btn"
+           @click="declare('online')">开始申报</p>
       </div>
       <Footer></Footer>
     </div>
@@ -124,17 +113,17 @@ import Footer from "@/views/footer.vue";
 export default {
   name: "SubmitAgree",
   components: { Nav, Footer },
-  data() {
+  data () {
     return {
       mainnav: "4",
       checked: false,
     };
   },
-  created() {
+  created () {
     console.log(this.$route.params);
   },
   methods: {
-    declare() {
+    declare () {
       if (!this.checked) {
         return false;
       }
@@ -149,7 +138,7 @@ export default {
           break;
       }
     },
-    agree_type(val) {
+    agree_type (val) {
       console.log(val);
     },
   },
@@ -212,7 +201,7 @@ export default {
         font-size: 16px;
         color: #666;
         line-height: 30px;
-        margin-bottom:20px;
+        margin-bottom: 20px;
       }
       .standard4 {
         font-size: 16px;
@@ -220,12 +209,12 @@ export default {
         line-height: 30px;
         margin-left: 15px;
       }
-      .standard_4{
-         font-size: 16px;
+      .standard_4 {
+        font-size: 16px;
         color: #666;
         line-height: 30px;
       }
-      .paragraph_13{
+      .paragraph_13 {
         font-size: 16px;
         color: #666;
         line-height: 30px;
@@ -234,8 +223,24 @@ export default {
         font-size: 16px;
         color: #666;
         line-height: 30px;
-      
       }
+    }
+    .remember {
+      margin-top: 90px;
+      text-align: center;
+    }
+    .submit_btn {
+      text-align: center;
+      margin: 0 auto;
+      margin-top: 30px;
+      cursor: pointer;
+      width: 214px;
+      height: 46px;
+      line-height: 46px;
+      background: #c8a461;
+      border-radius: 8px;
+      color: #fff;
+      font-size: 18px;
     }
   }
 }
