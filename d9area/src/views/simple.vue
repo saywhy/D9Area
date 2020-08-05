07f08760e9b77f564385fbc18e94e3766b62cf5a
zhongshuaisun <sun_zhong_shuai@163.com>
2020-08-05 11:53:09 +0800

    13212

--- d9area/src/views/simple.vue
+++ d9area/src/views/simple.vue
@@ -4,22 +4,28 @@
       <film-nav :activenav="Index"></film-nav>
       <div class="simple_box">
         <div>
-          <img src="@/assets/images/simple/jj.jpg"
-               alt
-               class="jj" />
+          <img src="@/assets/images/simple/jj.jpg" alt class="jj" />
         </div>
         <div class="simple_box_min">
           <!-- <img src="@/assets/images/simple/tc.png" alt class="tc" /> -->
           <p class="question">CG动画电影周的使命是什么？</p>
-          <p class="key"
-             align="left">首届D9区国际CG动画电影周分为竞赛单元、论坛单元、展映单元、青年动画导演扶持计划四大板块内容，首届主竞赛单元只接受VFX电影短片、三维动画短片、VR短片三个方向，面向全球CG短片创作者开放，邀请国内外影视、动画行业领军人士和著名导演担任评委，评选并奖励优秀CG短片的创作，发现、扶持有理想、有激情、有创造力的动画团队和人才，孵化优质短片项目， 促进国际文化交流与合作，创建CG短片创作、汇聚、交流、交易的国际化平台。
-          </p>
+          <p
+            class="key"
+          >D9区国际CG动画电影周（以下简称ICF）将评选并奖励优秀CG短片的创作，发现、扶持有理想、有激情、有创造力的动画团队和人才，孵化优质短片项目，促进国际文化交流与合作，创建CG短片创作、汇聚、交流、交易的国际化平台。</p>
           <p class="question">首届ICF会有哪些内容板块？</p>
-          <p class="key"
-             align="left">这个单元旨在通过多个个性鲜明的评选环节来凸显电影创作的多样化特点。一些入围竞赛单元的影片是“大众化作者电影”优秀的阐释，而一种注目单元则着重作品意图和审美的独特性，以通过全球荧幕来展示一种低调却强有力的美学风格。官方评选也涉及非竞赛单元（Hors Compétition）、特别场（Séances Spéciales）和午夜场（Séances de minuit），以及展现电影历史财富的戛纳经典（Cannes Classics） 和电影学校电影基金会（Cinéfondation） 评选。最重要的是，这个评选需要保持创意与地理区域的平衡，具有能够反映当代电影产业的代表性。</p>
+          <p
+            class="key"
+          >首届ICF分为竞赛单元、展映单元、论坛单元、创投计划四大板块内容，首届主竞赛单元只接受VFX电影短片、三维动画短片、VR短片三个方向，面向全球CG短片创作者开放。</p>
           <p class="question">竞赛单元的评选标准是什么？</p>
-          <p class="key"
-             align="left">这个单元旨在通过多个个性鲜明的评选环节来凸显电影创作的多样化特点。一些入围竞赛单元的影片是“大众化作者电影”优秀的阐释，而一种注目单元则着重作品意图和审美的独特性，以通过全球荧幕来展示一种低调却强有力的美学风格。官方评选也涉及非竞赛单元（Hors Compétition）、特别场（Séances Spéciales）和午夜场（Séances de minuit），以及展现电影历史财富的戛纳经典（Cannes Classics） 和电影学校电影基金会（Cinéfondation） 评选。最重要的是，这个评选需要保持创意与地理区域的平衡，具有能够反映当代电影产业的代表性。</p>
+          <p
+            class="key1"
+          >通过全CG三维制作的影片你要体现完整的叙事结构、独特的视角、鲜明的视觉风格和制作技术的突破。入围竞赛单元的影片将会是“全新时代CG影片”优秀的阐释，不仅着重用视觉讲故事，阐述作品意图和审美的独特性，以通过全CG三维制作来展示一种强有力的美学风格。获奖作品将会进入ICF电影基金会的创投计划中，以挖掘高品质影片创作为宗旨。</p>
+
+          <p
+            class="key"
+          >该单元将邀请国内外影视、动画行业领军人士和著名导演担任评委，首届D9区国际CG动画电影周的国际评审主席是彼得.拉姆齐（Peter Ramsey），他是《蜘蛛侠.平行宇宙》的导演，该作品获得奥斯卡最佳动画片奖。</p>
+          <p class="question">展映单元由什么组成？</p>
+          <p class="key">由ICF竞赛单元入围影片和主题展映邀约两部分组成。</p>
         </div>
       </div>
       <Footer></Footer>
@@ -34,7 +40,7 @@ import filmNav from "@/views/film_nav.vue";
 import filmTop from "@/views/film_top.vue";
 export default {
   components: { Nav, Footer, filmTop, filmNav },
-  data () {
+  data() {
     return {
       Index: "2",
       mainnav: "2",
@@ -77,11 +83,22 @@ export default {
   }
   .key {
     font-size: 18px;
-    margin-bottom: 40px;
+    margin-bottom: 30px;
     font-family: SimHei;
     font-weight: 400;
     color: #000;
     line-height: 30px;
+    text-align: left;
+    text-indent: 35px;
+  }
+  .key1{
+    font-size: 18px;
+    margin-bottom: 10px;
+    font-family: SimHei;
+    font-weight: 400;
+    color: #000;
+    line-height: 30px;
+    text-align: left;
     text-indent: 2em;
   }
 }
--- d9area/src/views/submit_agree.vue
+++ d9area/src/views/submit_agree.vue
@@ -3,36 +3,113 @@
     <Nav :mainnav="mainnav"></Nav>
     <div class="submit_agree">
       <div class="agree_banner">
-        <img src="@/assets/images/film/film_show_bg1.png" alt />
+        <img src="@/assets/images/film/film_show_bg1.jpg" alt />
       </div>
       <div class="main_container">
-        <!-- <p class="agree_title">影片提交协议</p> -->
-        <!-- <div class="hr"></div> -->
-        <div class="agree_content">
-          <p class="category">首届D9区国际CG动画电影周竞赛单元只接受短片，分为以下三个类别：VFX电影短片、CG动画短片、VR短片。</p>
-          <p class="title">(一)注册报名与作品提交时间：</p>
-          <p class="height">2020年8月10日～2020年10月30日。</p>
-          <p class="title">(二)首届D9区国际CG动画电影周奖项设置：</p>
-          <p class="height">最佳短片奖、最佳导演奖、最佳编剧奖、最佳剪辑奖、最佳艺术指导奖、最佳特效制作奖、最佳角色设计奖、最佳环境设计奖、最佳动效设计奖、最佳技术贡献奖</p>
-          <p class="title">(三)作品提交须知与规则</p>
-          <div class="rule">
-            <p class="height">1.所有类别的短片时长要求5-15min；</p>
-            <p>2.题材类型不限，须全CG三维制作；</p>
-            <p>3.字幕至少包含中文或英文字幕；</p>
-            <p>4.申报作品需是在近两年内完成的；</p>
-            <p>5.申报作品提交成功后，组委会原则上不再接受新的版本。如果报名信息有重大变化，请及时联系组委会；</p>
-            <p>6.申报作品必须为原创短片项目，要体现原创性、创新性、艺术性，要有较高的思想艺术内涵与价值；</p>
-            <p>7.申报者须保证拥有该作品的全部版权或持有版权方授权许可证明，证明其有权在ICF上宣传、放映和参加相关活动。因申报作品著作权、商标权及其他知识产权所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
-            <p>8.申报者须确保作品的申报符合所在国家/地区的法律法规要求，因申报方违反其所在国家/地区的法律法规所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
-            <p>9.申报作品的所有资料等应按主办方的要求提交，申报者同意并授权主办方无偿使用申报作品提供的所有材料，进行推广宣传、展映活动、媒体的各种媒介宣传使用的权利，仅限于对本活动和本片的宣传（如主办方要求，申报者需承诺提供相应授权文件）；</p>
-            <p>10.申报者所提交的所有材料均不可退还，请务必保留原件，并且一旦申请者接受提名，则不能退出评选，最终入围选手须给予主办方至少一次在北京区放映影片的权利；</p>
-            <p>11.入围作品的导演（制片人或制作团队的其他代表）将被邀请到北京参加为期一周的电影周及其他相关活动，包括映后谈、ICF论坛、青年导演扶持计划、颁奖典礼等。最终入围者需要独立承担赴中旅游签和旅游保险，主办方可根据需求提供邀请函；</p>
-            <p>12.本活动的最终解释权归主办方所有，主办方有权不经预先通知根据实际情况对推选活动章程中所涉及到的各项条款进行适当调整;</p>
+        <div class="big_title">D9区国际CG动画电影周章程</div>
+        <div class="big_title1">2020年</div>
+        <div class="regulations">
+          <p class="titles">第一条</p>
+          <p class="paragraph">本章程适用于报名参加第一届D9区国际CG动画电影周（以下简称“ICF”）主竞赛单元的影片。</p>
+          <p class="titles">第二条</p>
+          <p
+            class="paragraph"
+          >D9区国际CG动画电影周，是由xxx批准，中国电影美术学会CG艺术专业委员会与北京国际设计周联合主办的CG专业领域的大型竞赛型电影周。电影周的宗旨是评选并奖励优秀CG短片的创作，发现、扶持和激励有理想、有激情、有创造力的短片影视、动画团队和人才，孵化优质短片项目，促进国际文化交流与合作，创建CG短片创作、汇聚、交流、交易和投融资的国际性品牌平台。</p>
+          <!-- 第三条 -->
+          <p class="titles">第三条</p>
+          <p class="paragraph3">ICF每年12月举行。</p>
+          <p class="paragraph">第一届D9区国际CG动画电影周于2020年12月于北京举行。</p>
+          <!-- 第四条 -->
+          <p class="titles">第四条</p>
+          <p class="paragraph3">ICF负责评选和邀请影片参加主竞赛单元和展映单元。</p>
+          <!-- 标准 -->
+          <p class="paragraph3">主竞赛单元申报影片应符合下列标准：</p>
+          <p class="standard">1．首届主竞赛单元只接受VFX电影短片、三维动画短片、VR短片三个方向；</p>
+          <p class="standard">2．2018年8月1日以后制作完成的原创短片项目；</p>
+          <p class="standard">3．所有类别的短片时长要求5-17min；</p>
+          <p class="standard">4．题材类型不限，须全CG三维制作；</p>
+          <p class="standard">5．字幕至少包含中文或英文字幕；</p>
+          <p class="standard_4 ">6．申报作品提交内容：</p>
+          <!-- 内容 -->
+          <div class="content">
+            <p>（1）作品正片：5-17min；</p>
+            <p>（2）作品预告片/花絮：45-150s;</p>
+            <p>（3）作品制作解析：不限时长；</p>
+            <p>（4）作品简介：200字以内；</p>
+            <p>（5）作品海报：1-3张；</p>
+            <p>（6）作品封面（视频的封面显示）：1张；</p>
+            <p>（7）作品概念设定图：不限数量；</p>
+            <p>（8）作品故事板分镜图：不限数量。</p>
           </div>
-          <div class="remember">
-            <el-checkbox v-model="checked" @change="agree_type">我已阅读并同意遵守条例</el-checkbox>
-          </div>
-          <p class="submit_btn" :class="checked==true?'add_true':''" @click="declare">开始申报</p>
+          <p class="standard_4">7.申报作品提交格式</p>
+          <p class="standard_4">（1）VFX电影短片与CG动画短片：</p>
+          <p class="standard4">a.视频mp4格式，容量小于4G;</p>
+          <p class="standard4">b.所有图片jpg/gif/png格式，像素小于10M；</p>
+          <p class="standard_4">（2）VR短片：</p>
+          <p class="standard4">
+            a.视频360°全景mp4格式，容量小于4G，要求4K，可在PC或MAC平台上
+            使用UtoVR、VR Player、PotPlayer 、爱奇艺VR等支持本地播放的VR播放器
+            播放器观看；
+          </p>
+          <p class="standard4">b. 所有图片jpg/gif/png格式，像素小于10M；</p>
+
+          <p
+            class="standard"
+          >8．申报者须保证拥有该作品的全部版权或持有版权方授权许可证明，证明其有权在ICF上宣传、放映和参加相关活动。因申报作品著作权、商标权及其他知识产权所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
+          <p
+            class="standard"
+          >9．申报者须确保作品的申报符合所在国家/地区的法律法规要求，因申报方违反其所在国家/地区的法律法规所产生的任何法律纠纷或经济损失，由申报者承担全部法律责任，主办方及活动组织方免责；</p>
+
+          <p class="titles">第五条</p>
+          <p class="paragraph">请登录ICF网站www.di9qu.com，通过“影片申报”模块进行在线报名，并逐项填写申报影片的必填项信息。</p>
+          <p class="paragraph">申报影片的截止日期为2020年11月30日。</p>
+          <p
+            class="paragraph"
+          >ICF只接受官网在线申报影片，不接受其他移动储存介质或在线样片链接。申报影片的所有资料等应按主办方的要求提交，申报者同意并授权主办方无偿使用申报作品提供的所有材料，进行推广宣传、展映活动、媒体的各种媒介宣传使用的权利，ICF组委会承诺：申报影片的作有资料仅限于对本活动和本影片的宣传（如主办方要求，申报者需承诺提供相应授权文件）；</p>
+
+          <p class="titles">第六条</p>
+          <p class="paragraph">申报作品提交成功后，组委会原则上不再接受新的版本。如果报名信息有重大变化，请及时联系组委会；</p>
+          <p
+            class="paragraph"
+          >申报者所提交的所有材料均不可退还，请务必保留原件，申报影片在主竞赛单元入围后，即不得以任何理由擅自撤回影片，退出评选。如需撤回影片，ICF组委会有权根据实际情况决定是否接受。如因申报方提出撤回影片申请给ICF或ICF组委会造成任何损失的，申报方应当赔偿由此造成的一切经济损失及名誉损失，包括但不限于支付赔偿金，向电影周出具书面致歉信及在公众平台公开道歉等。最终入围选手须给予主办方至少一次在北京区放映影片的权利。</p>
+
+          <p class="titles">第七条</p>
+          <p class="paragraph">入围的影片，申报方需提供完整的中英文字幕台本。</p>
+          <p class="paragraph">少数民族语言或方言对白的华语影片，申报方须提供中英文字幕拷贝；</p>
+          <p class="paragraph">影片放映时配有电子中文字幕显示，英译中的翻译和字幕机操作由电影周负责。</p>
+
+          <p class="titles">第八条</p>
+          <p
+            class="paragraph"
+          >ICF邀请国内外电影专家组成主竞赛单元的评审委员会，对申报影片进行评选。入围影片将在颁奖典礼前公布，获奖作品的最终结果将在颁奖典礼上公布。 评委不能与竞赛影片的制作或商业开发有关。</p>
+
+          <p class="standard">评委会将评出下列奖项：</p>
+          <p class="standard">最佳短片奖</p>
+          <p class="standard">最佳导演奖</p>
+          <p class="standard">最佳编剧奖</p>
+          <p class="standard">最佳剪辑奖</p>
+          <p class="standard">最佳艺术指导奖</p>
+          <p class="standard">最佳特效制作奖</p>
+          <p class="standard">最佳角色设计奖</p>
+          <p class="standard">最佳环境设计奖</p>
+          <p class="standard">最佳动效设计奖</p>
+          <p class="standard">最佳技术贡献奖</p>
+
+          <p class="titles">第九条</p>
+          <p
+            class="paragraph"
+          >入围ICF竞赛单元作品的导演（制片人或制作团队的其他代表）将被邀请到北京参加为期一周的电影周及其他相关活动，包括映后谈、ICF论坛、青年导演扶持计划、颁奖典礼等。最终入围者需要独立承担赴中旅游签和旅游保险，主办方可根据需求提供邀请函。</p>
+
+          <p class="titles">第十一条</p>
+          <p class="paragraph">凡申报影片参加D9区国际CG动画电影周，即被视为承认并接受本章程。申报方有义务确保影片选送ICF的合法性。</p>
+
+          <p class="titles">第十二条</p>
+          <p class="paragraph">D9区国际CG动画电影周组委会保留对本章程条款的最终解释权，并有权处理本章程未涉及的一切事项。</p>
+
+          <p class="titles">第十三条</p>
+          <p class="paragraph_13">D9区国际CG动画电影周组委会联系方式：</p>
+          <p class="paragraph13">邮箱：111</p>
+          <p class="paragraph13">电话：111</p>
         </div>
       </div>
       <Footer></Footer>
@@ -90,57 +167,73 @@ export default {
     }
   }
   .main_container {
-    width:1200px;
+    width: 1200px;
     background-color: #fff;
-    // .hr {
-    //   border-bottom: 2px solid #292929;
-    // }
-    .agree_content {
-      .category {
-        font-size: 20px;
-        color: #141414;
-        margin-top: 40px;
-        text-align: left;
-      }
-      .title {
-        font-size: 20px;
-        color: #141414;
-        margin-top: 30px;
-        text-align: left;
-      }
-      .height {
-        font-size: 16px;
-        color: #666666;
-        margin-top: 10px;
-        text-align: left;
-      }
-      .rule {
-        font-size: 16px;
-        color: #666666;
-        line-height: 30px;
-        text-align: left;
-      }
+    margin-bottom: 70px;
+    .big_title {
+      font-size: 38px;
+      color: #141414;
+      margin: 30px 0 20px 0;
+    }
+    .big_title1 {
+      font-size: 38px;
+      color: #141414;
+      margin-top: 50px;
     }
 
-    .remember {
-      margin:72px 0 42px 0;
-      .el-checkbox__label {
-        font-size: 16px;
+    .regulations {
+      color: #141414;
+      text-align: left;
+      margin-top: 80px;
+      .titles {
+        font-size: 24px;
+        margin-bottom: 10px;
       }
-    }
-    .submit_btn {
-      margin: 0 auto;
-      width: 200px;
-      height: 58px;
-      font-size: 18px;
-      line-height: 58px;
-      background: #3b3b3b;
-      border-radius: 8px;
-      margin-bottom: 70px;
-    }
-    .add_true {
-      background: #C8A461;
-      cursor: pointer;
+      .paragraph {
+        font-size: 18px;
+        color: #666;
+        line-height: 30px;
+        margin-bottom: 20px;
+      }
+      .paragraph3 {
+        font-size: 18px;
+        color: #666;
+        margin-bottom: 10px;
+      }
+      .standard {
+        font-size: 16px;
+        color: #666;
+        line-height: 30px;
+        margin-bottom: 20px;
+      }
+      .content {
+        font-size: 16px;
+        color: #666;
+        line-height: 30px;
+        margin-bottom:20px;
+      }
+      .standard4 {
+        font-size: 16px;
+        color: #666;
+        line-height: 30px;
+        margin-left: 15px;
+      }
+      .standard_4{
+         font-size: 16px;
+        color: #666;
+        line-height: 30px;
+      }
+      .paragraph_13{
+        font-size: 16px;
+        color: #666;
+        line-height: 30px;
+      }
+      .paragraph13 {
+        font-size: 16px;
+        color: #666;
+        line-height: 30px;
+      
+      }
     }
   }
 }
